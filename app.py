fe1d1ab2727ba3a7210913f10baa7d9973d776d4
ManishMeena788 <119215519+ManishMeena788@users.noreply.github.com>
2022-11-29 21:51:29 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask,render_template,request,redirect
 import time
+import threading
 
 server = Flask(__name__)
 
-while True:
-  print("akhil")
-  print("kyu")
-  time.sleep(10)
+def akhil:
+  while True:
+    print("akhil")
+    print("kyu")
+    time.sleep(10)
 
 
 
@@ -16,7 +18,7 @@ def getMessage():
   return("running")
 
 if __name__ == "__main__":
-  #threading.Thread(target=runAutoList, name='run_server_time', daemon=True).start()
+  threading.Thread(target=akhil, name='run_server_time', daemon=True).start()
   #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
   #server.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 8080)))
   server.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 1000)))
