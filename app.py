26c1b3f3497b279138e1c4f82b96e40d1841547d
ManishMeena788 <119215519+ManishMeena788@users.noreply.github.com>
2022-11-29 22:44:59 +0530

    update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ server = Flask(__name__)
 def start(message):
   bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-def akhil():
+def akhil1():
   a=0
   while True:
     a+=1
@@ -37,7 +37,7 @@ def webhook():
 def GetMuessageyu():
   return("running")
 
-threading.Thread(target=akhil, name='run_server_time', daemon=True).start()
+threading.Thread(target=akhil1, name='run_server_time', daemon=True).start()
 
 if __name__ == "__main__":
   #server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
