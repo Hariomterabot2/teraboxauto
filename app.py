3ef8099b16bc8bae492523d687669a2e240233be
ManishMeena788 <119215519+ManishMeena788@users.noreply.github.com>
2022-11-29 22:07:25 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,18 +2,38 @@ import os
 from flask import Flask,render_template,request,redirect
 import time
 import threading
+import telebot
 
+
+TOKEN = '5072766021:AAHN57wjgpEG1c6OSqoMeYXbRBdbeGgElYs'
+bot = telebot.TeleBot(TOKEN)
+#app=Flask(__name__)
 server = Flask(__name__)
+
+ 
+@bot.message_handler(commands=['start'])
+def start(message):
+  bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 def akhil():
   while True:
+    bot.send_message(chat_id="")
     print("akhil")
     print("kyu")
     time.sleep(10)
 
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+ 
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://' + 'alivebots' + '.onrender.com/' + f"{TOKEN}")
+    return "!", 200
 
-
-@server.route('/')
+@server.route('/home')
 def getMessage():
   return("running")
 
