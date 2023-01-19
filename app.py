084feae783eedc51aa98dc15e113ce446ad32e87
ManishMeena788 <119215519+ManishMeena788@users.noreply.github.com>
2023-01-19 13:58:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,5 +110,5 @@ def webhook():
     return "!", 200
  
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 1000)))
+    server.run(debug=True,host="0.0.0.0", port=int(os.environ.get('PORT', 1000)))
  
