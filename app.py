7d3ef073827bfd0e84ce93db9b6748d10928913a
Badnaamchora <121307882+Badnaamchora@users.noreply.github.com>
2023-01-21 17:01:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,7 +123,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://alivebots.onrender.com/' + f"{API_TOKEN}")
+    bot.set_webhook(url='https://ManishTera.onrender.com/' + f"{API_TOKEN}")
     return "!", 200
  
 if __name__ == "__main__":
