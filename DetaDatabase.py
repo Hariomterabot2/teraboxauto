3eb82a89993f15b56f162bbfb1b57027916aee2f
Badnaamchora <121307882+Badnaamchora@users.noreply.github.com>
2023-01-21 18:02:23 +0530

    Update DetaDatabase.py

--- DetaDatabase.py
+++ DetaDatabase.py
@@ -17,7 +17,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("Badnaamchora2784.json", scope)
 client = gspread.authorize(creds)
-ak = client.open("Manishforward")
+ak = client.open("ManishForward")
 general = ak.worksheet("general")
 channels = ak.worksheet("channels")
 
