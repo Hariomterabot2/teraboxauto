a52b545465b05e8ed50e6aacd5abfaa4b5ce7f88
Dileepsinghrawat <87635025+Dileepsinghrawat@users.noreply.github.com>
2023-02-02 22:26:40 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 POSTCHANNEL = -1001898159485
 GENERALCHANNEL = "https://t.me/+DptrL_kyEEw5YWU1"
 PostText ="""<b>{}🥰
-📱 𝐖𝐚𝐭𝐜𝐡 𝐎𝐧𝐥𝐢𝐧𝐞 👨‍💻 
   
 {}
 {}
   
-🔥🔥Must watch Guys🔥🔥
 🛑Login to watch full video🛑
 ━━━━━━━━━━━━━━━━━━━━━
 ✅ ᴊᴏɪɴ ɴᴏᴡ ꜰᴏʀ ᴍᴏʀᴇ ᴠɪᴅᴇᴏꜱ!
