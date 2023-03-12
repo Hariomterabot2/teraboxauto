b7dd496965e4be1e76bbd0873a96fc95b7cacde7
ultroxgamer <127664754+ultroxgamer@users.noreply.github.com>
2023-03-12 17:34:47 +0530

    update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG) 
 
 
-API_TOKEN = '1914052696:AAEH1HaCxNhjxtCee_ySBbpLLWxiCY6rV3g'
+API_TOKEN = '1921522398:AAFdwDnD-Odv5XdP7sxTosn3nRspkEnetZI'
 bot = telebot.TeleBot(API_TOKEN)
 server = Flask(__name__)
 
