3aecf41d9e851a571f41e0b668bd44009837424d
Hariomterabot2 <pawanrawat3301@gmail.com>
2024-05-20 22:25:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://hariomauto-735eef8dc630.herokuapp.com/' + f"{API_TOKEN}")
+    bot.set_webhook(url='https://teraboxauto-9rzp.onrender.com/' + f"{API_TOKEN}")
     return "!uk", 200
  
 if __name__ == "__main__":
